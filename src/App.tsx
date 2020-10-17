5b6b48e13564ec482b080a3c5bd700ba1f61bd3a
Danny Dwi Cahyono <70502753+dannycahyo@users.noreply.github.com>
2020-10-17 23:48:57 +0700

    feat: add menu (#6)
    * add-menu
    
    * fix-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
-import { Layout } from 'antd';
+import { Layout } from "antd";
+import { Menu } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
-    <div className="App">
+    <Layout style={{ minHeight: '100vh' }}>
+      <Layout.Sider>
+        <Menu
+          mode="inline"
+          theme="dark"
+        >
+          <Menu.Item>Option 1</Menu.Item>
+          <Menu.Item>Option 2</Menu.Item>
+        </Menu>
+      </Layout.Sider>
       <Layout>
-        <Layout>
-          <Layout.Sider>ini sider</Layout.Sider>
-        </Layout>
         <Layout.Header>ini header</Layout.Header>
         <Layout.Content>ini content</Layout.Content>
         <Layout.Footer>ini footer</Layout.Footer>
       </Layout>
-    </div>
+    </Layout>
   );
 }
 
