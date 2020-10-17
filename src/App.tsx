df330da0eb97553b276bffefab9611bdae3c8285
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2020-10-18 04:43:49 +0700

    install_antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {DatePicker} from "antd";
 
 function App() {
   return (
     <div className="App">
+      (<DatePicker/>)
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
