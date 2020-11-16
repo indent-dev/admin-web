c8b13b0537e3bc98832f27f1d8d772de88914efe
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2020-11-16 16:48:51 +0700

    making-table

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,45 @@ import './App.css';
 import { Layout } from "antd";
 import { Menu } from "antd";
 import "antd/dist/antd.css";
+import { Table } from "antd"
+import { ColumnsType } from 'antd/lib/table';
+
+type Category = {
+  name: string
+}
 
 function App() {
+
+  const colums: ColumnsType<Category> = [
+    {
+      title: "No",
+      render: (text, record, index: number) => {
+        return index + 1
+      }
+    },
+    {
+      title: "Categories",
+      dataIndex: "name",
+    },
+  ]
+  const data: Category[] = [
+    {
+      name: "Pakaian Pria"
+    },
+    {
+      name: "Komputer & Aksesories"
+    },
+    {
+      name: "Perawatan & Kecantikan"
+    },
+    {
+      name: "Perlengkapan Rumah"
+    },
+    {
+      name: "Elektronik"
+    },
+  ]
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Layout.Sider>
@@ -18,7 +55,9 @@ function App() {
       </Layout.Sider>
       <Layout>
         <Layout.Header>ini header</Layout.Header>
-        <Layout.Content>ini content</Layout.Content>
+        <Layout.Content>
+          <Table columns={colums} dataSource={data} />
+        </Layout.Content>
         <Layout.Footer>ini footer</Layout.Footer>
       </Layout>
     </Layout>
