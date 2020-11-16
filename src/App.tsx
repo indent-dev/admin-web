b7cce3a440d4d4ac880c9909bbe34f61f93dc543
Danny Dwi Cahyono <70502753+dannycahyo@users.noreply.github.com>
2020-11-16 16:59:48 +0700

    feat: making table category (#10)
    * making-table
    
    * fix Aka's suggestion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import './App.css';
-import { Layout } from "antd";
-import { Menu } from "antd";
+import { Menu, Table, Layout } from "antd";
 import "antd/dist/antd.css";
+import { ColumnsType } from 'antd/lib/table';
+
+type Category = {
+  name: string
+}
 
 function App() {
+
+  const colums: ColumnsType<Category> = [
+    {
+      title: "No",
+      render: (text, record, index: number) => {
+        return index + 1
+      }
+    },
+    {
+      title: "Categories",
+      dataIndex: "name",
+    },
+  ]
+  const data: Category[] = [
+    {
+      name: "Pakaian Pria"
+    },
+    {
+      name: "Komputer & Aksesories"
+    },
+    {
+      name: "Perawatan & Kecantikan"
+    },
+    {
+      name: "Perlengkapan Rumah"
+    },
+    {
+      name: "Elektronik"
+    },
+  ]
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Layout.Sider>
@@ -18,7 +53,9 @@ function App() {
       </Layout.Sider>
       <Layout>
         <Layout.Header>ini header</Layout.Header>
-        <Layout.Content>ini content</Layout.Content>
+        <Layout.Content>
+          <Table columns={colums} dataSource={data} />
+        </Layout.Content>
         <Layout.Footer>ini footer</Layout.Footer>
       </Layout>
     </Layout>
