5cd011cda50311a69ef7ccb16f47e2f3c1c75218
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-04-13 17:24:08 +0700

    utilize-react-query & add-env

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,10 @@
 import React from "react"
-import { render, screen } from "@testing-library/react";
+import { QueryClient,QueryClientProvider } from "react-query"
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import App from "./App"
 import userEvent from "@testing-library/user-event"
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import {
-  requestGetCategory,
-  requestCreateCategory,
-  requestDeleteCategory,
-  requestEditCategory,
-} from './useFetchCategory';
 
 global.matchMedia =
   global.matchMedia ||
@@ -28,7 +23,7 @@ global.matchMedia =
           ctx.status(200),
           ctx.json([
             {
-              isDeleted: true,
+              isDeleted: false,
               _id: '5fb75b51a517ff355c71f46f',
               name: 'ATK ',
               createdAt: '2020-11-20T05:59:45.536Z',
@@ -36,7 +31,7 @@ global.matchMedia =
               __v: 0,
             },
             {
-              isDeleted: true,
+              isDeleted: false,
               _id: '5fba34e11cba4e40c88a2dfd',
               name: 'Peralatan Dapur',
               createdAt: '2020-11-22T09:52:33.682Z',
@@ -85,76 +80,38 @@ afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
 
 
+
+
 it("Should Just Render The App", async () => {
-    render (<App />)
+  const queryClient = new QueryClient()
+    render (
+    <QueryClientProvider client={queryClient}>
+      <App />      
+    </QueryClientProvider>
+    )
+
+    await waitForElementToBeRemoved(()=> screen.getByText("Wait a second"))
+    expect(screen.getByText("ATK")).toBeInTheDocument()
+    expect(screen.getByText("Peralatan Dapur")).toBeInTheDocument()
 
     const input = screen.getByPlaceholderText("Create New Category")
     const value = "Perlengkapan Sekolah"
     userEvent.type(input,value)
     userEvent.click(screen.getByRole("button", { name:"Add Category" }))
-
-      const getCategory = await requestGetCategory();
-      expect(getCategory).toEqual([
-        {
-          isDeleted: true,
-          _id: '5fb75b51a517ff355c71f46f',
-          name: 'ATK ',
-          createdAt: '2020-11-20T05:59:45.536Z',
-          updatedAt: '2020-12-10T08:38:26.570Z',
-          __v: 0,
-        },
-        {
-          isDeleted: true,
-          _id: '5fba34e11cba4e40c88a2dfd',
-          name: 'Peralatan Dapur',
-          createdAt: '2020-11-22T09:52:33.682Z',
-          updatedAt: '2020-12-10T08:14:49.102Z',
-          __v: 0,
-        },
-      ]);
     
-      server.use(
-        rest.get(
-          'https://product-service-indent.herokuapp.com/category',
-          (req, res, ctx) => {
-            return res(ctx.status(404));
-          }
-        )
-      );
-      await expect(requestGetCategory()).rejects.toThrow(Error);
-
-      const postCategory = await requestCreateCategory('Yolloo');
-      expect(postCategory).toEqual('Yollo');
-      
-      const deleteCategory = await requestDeleteCategory({
-        name: 'ATK ',
-        id: '5fb75b51a517ff355c71f46f',
-      });
-      expect(deleteCategory).toEqual({
-        name: 'ATK ',
-        id: '5fb75b51a517ff355c71f46f',
-      });
-
-
-      const editCategory = await requestEditCategory({
-        name: 'Perlengkapan Sekolah',
-        id: '5fba34e11cba4e40c88a2dfd',
-      });
-      expect(editCategory).toEqual({
-        name: 'Perlengkapan Sekolah',
-        id: '5fba34e11cba4e40c88a2dfd',
-      });
-
+    // userEvent.type(screen.getByRole("textbox", { name: "Create New Category" }),"")
+    // await waitForElementToBeRemoved(() => screen.getByText("Perlengkapan Sekolah"))
+    // expect(await screen.findByText("Perlengkapan Sekolah")).toBeInTheDocument() 
+    
     userEvent.click(screen.getAllByRole("button", { name:"Edit" })[0])
     userEvent.type(screen.getByRole("textbox", { name: "Edit Category" }),"Yuhuuuu")
-    userEvent.click(screen.getByRole("button", { name: "OK" }))
-
-    userEvent.click(screen.getAllByRole("button", { name:"Edit" })[0])
     userEvent.click(screen.getByRole("button", { name: "Cancel" }))
 
-    userEvent.click(screen.getAllByRole("button", { name:"Delete" })[0])
+    userEvent.click(screen.getAllByRole("button", { name:"Edit" })[0])
+    userEvent.click(screen.getByRole("button", { name: "OK" }))
 
+    userEvent.click(screen.getAllByRole("button", { name:"Delete" })[0])
+    // expect(screen.getByText("ATK")).not.toBeInTheDocument()
 
     screen.debug()
-
   })
